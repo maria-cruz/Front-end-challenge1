d2976a877cef237dafa530efe3caf09141f2a488
Teresa Cruz <teresa.cruz@ubidy.com>
2022-01-19 22:08:28 +0800

    changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import joyRecommendation from "../images/joy.jpeg";
 import camilleRecommendation from "../images/cams.jpeg";
-import maridinRecommendation from "../images/maridin.jpeg";
+import karenRecommendation from "../images/karen.jpeg";
 import jerryRecommendation from "../images/jerry.jpeg";
 import bastiRecommendation from "../images/basti.jpeg";
+import maridinRecommendation from "../images/maridin.jpeg";
+import chelzRecommendation from "../images/chelz.jpeg";
 const About = () => {
   return (
     <section className="about-section">
@@ -56,6 +58,31 @@ const About = () => {
           </p>
         </div>
 
+        <div className="chelz-bg recommendations-item">
+          <div className="profile-info-wrapper">
+            <img
+              className="profile-img"
+              src={chelzRecommendation}
+              alt="chelz.jpeg"
+            />
+            <div className="profile-info">
+              <div className="name-container">Chelzea Louise Salvador</div>
+              <div className="job-position">Senior Software Engineer</div>
+            </div>
+          </div>
+          <div className="remarks-container">
+            Her contribution is valuable to the side, and I highly recommend
+            Teresa.
+          </div>
+          <p className="expanded-remarks">
+            "There is no better colleague than Teresa. She is one of the most
+            dedicated professionals I’ve worked with and is willing to put that
+            extra help whenever you need it. Her expertise as a software
+            engineer is considerable, and it helped our team come up with more
+            efficient solutions."
+          </p>
+        </div>
+
         <div className="maridin-bg recommendations-item">
           <div className="profile-info-wrapper">
             <img
@@ -68,12 +95,35 @@ const About = () => {
               <div className="job-position">Senior Software Engineer</div>
             </div>
           </div>
+
+          <p className="expanded-remarks">
+            "Guiding her in training went well because she's hard-working and
+            willing to take challenges."
+          </p>
+        </div>
+
+        <div className="karen-bg recommendations-item">
+          <div className="profile-info-wrapper">
+            <img
+              className="profile-img"
+              src={karenRecommendation}
+              alt="maridin.jpeg"
+            />
+            <div className="profile-info">
+              <div className="name-container">Karen Nipin</div>
+              <div className="job-position">Software Engineer</div>
+            </div>
+          </div>
           <div className="remarks-container">
-            Teresa is a hard-working and very talented individual.
+            Teresa is not only a fantastic and precise sofware engineer but also
+            an inspiring person.
           </div>
           <p className="expanded-remarks">
-            "I've been working with her for the past 2 years. She's good at her
-            work, and we always finish the tasks before the deadlines."
+            "She is very thorough in everything she does and can be depended
+            upon to get the job done. Excellent and result driven. She works
+            towards maximal customer satisfaction. She showed a high level of
+            react, scss, typescript technical skills and was a valuable
+            contributor to our projects."
           </p>
         </div>
 
@@ -109,7 +159,7 @@ const About = () => {
             />
             <div className="profile-info">
               <div className="name-container">Sebastian Marcelo</div>
-              <div className="job-position">Software Engineer</div>
+              <div className="job-position">Senior Software Engineer</div>
             </div>
           </div>
           <div className="remarks-container">
